4cdb416d20e17bc74e18bfce8ffc06852f5196b8
HaKIM <webdevhakim@gmail.com>
2023-07-20 21:02:31 +0200

    Moved to testcontainers and local environment for github actions

--- src/test/kotlin/com/hakim/SparklesTest.kt
+++ src/test/kotlin/com/hakim/SparklesTest.kt
@@ -2,9 +2,9 @@ package com.hakim
 
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import jakarta.inject.Inject
 
 @MicronautTest
 class SparklesTest {
@@ -16,5 +16,4 @@ class SparklesTest {
     fun testItWorks() {
         Assertions.assertTrue(application.isRunning)
     }
-
 }
