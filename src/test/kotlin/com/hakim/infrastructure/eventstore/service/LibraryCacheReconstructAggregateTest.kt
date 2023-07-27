a5625e44ab581467c199e72f407074f0f54a929c
HaKIM <webdevhakim@gmail.com>
2023-07-27 20:17:30 +0200

    Unit tests for MongoEventStoreCacheProxy

--- src/test/kotlin/com/hakim/infrastructure/eventstore/service/LibraryCacheReconstructAggregateTest.kt
+++ src/test/kotlin/com/hakim/infrastructure/eventstore/service/LibraryCacheReconstructAggregateTest.kt
@@ -1,16 +1,15 @@
-package com.hakim.infrastructure.eventstore.proxy
+package com.hakim.infrastructure.eventstore.service
 
 import com.hakim.domain.AggregateId
 import com.hakim.domain.Library
 import com.hakim.domain.service.LibraryReconstruction
 import com.hakim.infrastructure.eventstore.EventStore
-import com.hakim.infrastructure.eventstore.service.LibraryCacheReconstructAggregate
 import io.github.reactivecircus.cache4k.Cache
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.util.*
 
@@ -30,7 +29,7 @@ class LibraryCacheReconstructAggregateTest {
 
         val result = proxy.reconstruct(testAggregateId)
 
-        assertEquals(testLibrary, result)
+        Assertions.assertEquals(testLibrary, result)
         coVerify(exactly = 0) { mockEventStore.read(testAggregateId) }
     }
 }
